479a6582b66b75472fe94a184c09e1c497392217
voodoo <voodoo@blackandyellowpages.com>
2014-08-12 16:49:31 -0600

    no chromedriver support for polymer testing

--- test/e2e/appShellSpec.js
+++ test/e2e/appShellSpec.js
@@ -1,55 +1,74 @@
+
+//var retry = require('webdriverjs-retry');
+
 var Elements = function () {
   //this.applicationContainer = element(by.id('applicationContainer'));
   this.toolTitle = element(by.id('toolTitle'));
 };
 
 var elements;
+var toolTitle;
+
+var waitForElement = function (elm, txt, attempts) {
+  if (attempts == null) {
+    attempts = 3;
+  };
+
+  return browser.driver.findElement(elm).then(function (found) {
+    browser.manage().timeouts().implicitlyWait(300);
+    return browser.wait(function () {
+      return found.isDisplayed().then(function (visible) {
+        if (visible) {
+          return found(getText()).then(function (gotText) {
+            return gotText === txt;
+          });
+        } else {
+          browser.sleep(300);
+          return false;
+        };
+      }, function (err) {
+        if (attempts > 0) {
+          return waitForElement(elm, txt, attempts -1 );
+        } else {
+          throw err;
+        };
+      });
+    }, 5000, 'Expectation error: waiting for element to getText().');
+  }, function (err) {
+    if (attempts > 0) {
+      return waitForElement(elm, txt, attempts -1);
+    } else {
+      throw err;
+    };
+  });
+};
 
 describe("html5 application shell", function () {
-/*
-  it('loads the page and waits for polymer-ready', function(done){
-    browser.get('index.html');
-    document.
-  })
-*/
-  it("sets up the tests", function () {
-    /*browser.get('/').then(function () {
-      console.log('sleeping');
-      browser.driver.sleep(5000).then(function () {
-        console.log('awake');
-        elements = new Elements();
-        expect(elements.toolTitle.isDisplayed()).toBeTruthy();
-        expect(elements.toolTitle.getText()).toEqual('SocietyPro Groups and Governanance');
-      });
-    });*/
-    /*browser.wait(function () {
-      document.
-    });*/
-    var localReady;
 
-    browser.get('/');
+  browser.get('/');
+  var POLYMER_READY = false;
+  
+  beforeEach(function (done) {
     browser.wait(function () {
-      console.log('waiting for polymer');
-      browser.executeScript(function () {
+      POLYMER_READY = browser.executeScript(function () {
         return soProTestsReady == true;
       });
+      return POLYMER_READY
     });
+  });
 
-    browser.get('/').then(function () {
-      browser.wait(function(){
-        console.log('waiting for polymer');
-        localReady = browser.executeScript(function () {
-          return soProTestsReady == true;
-        });
-        return localReady;
-      }).then(function () {
-        console.log('done waiting');
-        console.log(localReady);
-        elements = new Elements();
-        expect(elements.toolTitle.isDisplayed()).toBeTruthy();
-        expect(elements.toolTitle.getText()).toEqual('SocietyPro Groups and Governanance');
-      });
-    });
+/*  it("has an application container", function () {
+    var applicationContainer = element(by.id('applicationContainer'));
+    expect(applicationContainer.isDisplayed()).toBeTruthy();
+  });
+*/
+
+  it("has a title bar", function () {
+    var toolTitle = element(by.id('toolTitle'));
+    if (waitForElement({id: 'toolTitle'}, 'SocietyPro Groups and Governance', 25)) {
+      expect(toolTitle.isDisplayed()).toBeTruthy();
+      expect(toolTitle.getText()).toEqual('SocietyPro Groups and Governance');
+    }
   });
 
 });
