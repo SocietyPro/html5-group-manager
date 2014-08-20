afbd8537bddc235f693b6d6feb84c614f65b2fcd
plato-cambrian <plato@cambrian.org>
2014-08-20 18:33:26 -0400

    Try polymer data binding

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
+var Cambrian = Cambrian || {};
+var japi;
+if(Cambrian.JAPI !== undefined && !Cambrian.isMockCambrian){
+  console.log('Instantiating japi');
+  japi = Cambrian.JAPI();
+} else {
+  // use mocks
+  console.log('Instantiating mock japi');
+  japi = Cambrian.mockJAPI();
+}
+
+//appModule = angular.module("app", ['eee-c.angularBindPolymer'])
+
 appModule = angular.module("app", [])
 .controller("appCtrl", function ($scope) {
-  // controller logic go here
+  $scope.tempScopeName = "Just a test";
+  $scope.tempScope = {name: "Just a test"};
+  $scope.myPeers = japi.me.peers;
+  $scope.myGroups = japi.me.groups;
 });
