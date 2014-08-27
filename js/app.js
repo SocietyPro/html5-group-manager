8b35fdde9562b5f9f26218ac9ac9ae801982ff3f
voodoo <voodoo@blackandyellowpages.com>
2014-08-26 22:33:03 -0600

    porting to angularjs web-elements

--- js/app.js
+++ js/app.js
@@ -11,10 +11,72 @@ if(Cambrian.JAPI !== undefined && !Cambrian.isMockCambrian){
 
 //appModule = angular.module("app", ['eee-c.angularBindPolymer'])
 
-appModule = angular.module("app", [])
-.controller("appCtrl", function ($scope) {
-  $scope.tempScopeName = "Just a test";
-  $scope.tempScope = {name: "Just a test"};
+appModule = angular.module("app", ['ngMaterial'])
+.factory("menu", ['$rootScope', function ($rootScope) {
+  var self;
+  var groups = ['myGroups', 'myPeerLists'];
+
+  return self = {
+    groups: groups,
+
+    selectGroup: function(group) {
+      self.currentGroup = group;
+      $rootScope.listContains = group;
+    },
+    isGroupSelected: function (group) {
+      return self.currentGroup === group;
+    }
+  };
+
+}])
+.controller("appCtrl", function ($scope, $materialSidenav, menu, $rootScope) {
+  
+  $scope.menu = menu;
+  $scope.menu.selectGroup(menu.groups[0]);
+
   $scope.myPeers = japi.me.peers;
   $scope.myGroups = japi.me.groups;
+  $scope.myPeerLists = japi.me.peerLists;
+
+  for (var i=0; i < $scope.myGroups.length; i++) {
+    $scope.myGroups[i].isActive = false;
+  };
+
+  /*
+  for (var i=0; i < $scope.myPeerLists.length; i++) {
+    $scope.myGroups.isActive[i] = false;
+  };
+  */
+
+  $scope.toggleMenu = function () {
+    $materialSidenav('left').toggle();
+  };
+
+  $scope.listView = "stream";
+
+  $scope.streamView = function () {
+    $scope.listView = "stream";
+  };
+
+  $scope.quiltView = function () {
+    $scope.listView = "quilt";
+  };
+
+  $scope.toggleEntry = function (entry) {
+    entry.isActive = !entry.isActive;
+  };
+})
+.directive('ig', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      fid: '@'
+    },
+    template: 
+      '<material-input-group>' +
+        '<label for="{{fid}}">Add a group...</label>' +
+        '<material-input id="{{fid}}" type="text" ng-model="data.description">' +
+      '</material-input-group>'
+  };
 });
