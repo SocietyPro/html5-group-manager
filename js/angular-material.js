dfecf9388542c3377aefcfc8f0e033637407c767
voodoo <voodoo@blackandyellowpages.com>
2014-08-27 09:28:42 -0600

    changed peers to members

--- js/angular-material.js
+++ js/angular-material.js
@@ -3,7 +3,7 @@
  * WIP Banner
  */
 (function(){
-angular.module('ngMaterial', [ 'ng', 'ngAnimate', 'material.services.attrBind', 'material.services.compiler', 'material.services.position', 'material.services.registry', 'material.services.throttle', "material.components.button","material.components.card","material.components.checkbox","material.components.content","material.components.dialog","material.components.form","material.components.groupcard","material.components.icon","material.components.iconbutton","material.components.list","material.components.radioButton","material.components.sidenav","material.components.slider","material.components.tabs","material.components.toast","material.components.toolbar","material.components.whiteframe"]);
+angular.module('ngMaterial', [ 'ng', 'ngAnimate', 'material.services.attrBind', 'material.services.compiler', 'material.services.position', 'material.services.registry', 'material.services.throttle', "material.components.button","material.components.card","material.components.checkbox","material.components.content","material.components.customform","material.components.dialog","material.components.form","material.components.groupcard","material.components.icon","material.components.iconbutton","material.components.list","material.components.radioButton","material.components.sidenav","material.components.slider","material.components.tabs","material.components.toast","material.components.toolbar","material.components.whiteframe"]);
 angular.module('ngAnimateSequence', ['ngAnimate'])
 
   .factory('$$animateAll', function() {
@@ -4029,7 +4029,7 @@ function materialGroupcardDirective() {
       $scope.toggleCard = function () {
         $scope.group.isActive = !$scope.group.isActive;
         $element.addClass('active');
-        $scope.avatar = "material-icons/icons/system_icons/social/res/2x_web/ic_person_48dp.png"
+        $scope.avatar = "/img/list/60.jpeg"
       };
     },
     template:
@@ -4037,12 +4037,12 @@ function materialGroupcardDirective() {
         '<h2>{{group.name}}</h2>' +
         '<div class="animate-show" ng-show="group.isActive">' +
           '<material-list>' +
-            '<material-item ng-repeat="peer in group.peers">' +
+            '<material-item ng-repeat="member in group.members">' +
               '<div class="material-tile-left">' +
                 '<img ng-src={{avatar}} class="face">' +
               '</div>' +
               '<div class="material-tile-content">' +
-                '<h2>{{peer.name}}</h2>' +
+                '<h2>{{member.name}}</h2>' +
               '</div>' +
             '</material-item>' +
           '</material-list>' +
@@ -4131,6 +4131,110 @@ function MaterialIconButtonDirective(ngHrefDirectives) {
     }
   };
 
+}
+
+/**
+ * @ngdoc module
+ * @name material.components.customform
+ * @description
+ * Form
+ */
+angular.module('material.components.customform', [])
+  .directive('materialCustomInputGroup', [
+    materialCustomInputGroupDirective
+  ])
+  .directive('materialCustomInput', [
+    materialCustomInputDirective
+  ]);
+
+/**
+ * @ngdoc directive
+ * @name materialCustomInputGroup
+ * @module material.components.customform
+ * @restrict E
+ * @description
+ * Use the `<material-custom-input-group>` directive as the grouping parent of an `<material-custom-input>` elements
+ *
+ * @usage 
+ * <hljs lang="html">
+ * <material-custom-input-group>
+ *   <material-custom-input type="text" ng-model="myText" size="200">
+ * </material-custom-input-group>
+ * </hljs>
+ */
+function materialCustomInputGroupDirective() {
+  return {
+    restrict: 'CE',
+    controller: ['$element', function($element) {
+      this.setFocused = function(isFocused) {
+        $element.toggleClass('material-input-focused', !!isFocused);
+      };
+      this.setHasValue = function(hasValue) {
+        $element.toggleClass('material-input-has-value', !!hasValue);
+      };
+    }]
+  };
+}
+
+/**
+ * @ngdoc directive
+ * @name materialCustomInput
+ * @module material.components.customform
+ *
+ * @restrict E
+ *
+ * @description
+ * Use the `<material-custom-input>` directive as elements within a `<material-custom-input-group>` container
+ *
+ * @usage
+ * <hljs lang="html">
+ * <material-custom-input-group>
+ *   <material-custom-input type="text" ng-model="user.fullName">
+ *   <material-custom-input type="text" ng-model="user.email">
+ * </material-custom-input-group>
+ * </hljs>
+ */
+function materialCustomInputDirective() {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<input>',
+    require: ['^?materialCustomInputGroup', '?ngModel'],
+    link: function(scope, element, attr, ctrls) {
+      var inputGroupCtrl = ctrls[0];
+      var ngModelCtrl = ctrls[1];
+      if (!inputGroupCtrl) {
+        return;
+      }
+
+      // When the input value changes, check if it "has" a value, and 
+      // set the appropriate class on the input group
+      if (ngModelCtrl) {
+        //Add a $formatter so we don't use up the render function
+        ngModelCtrl.$formatters.push(function(value) {
+          inputGroupCtrl.setHasValue(!!value);
+          return value;
+        });
+      }
+      element.on('input', function() {
+        inputGroupCtrl.setHasValue(!!element.val());
+      });
+
+      // When the input focuses, add the focused class to the group
+      element.on('focus', function(e) {
+        inputGroupCtrl.setFocused(true);
+      });
+      // When the input blurs, remove the focused class from the group
+      element.on('blur', function(e) {
+        inputGroupCtrl.setFocused(false);
+      });
+
+      scope.$on('$destroy', function() {
+        inputGroupCtrl.setFocused(false);
+        inputGroupCtrl.setHasValue(false);
+      });
+    }
+  };
 }
 
 angular.module('material.services.attrBind', [
