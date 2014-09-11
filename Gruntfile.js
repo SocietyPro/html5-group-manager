a570fd07e3787304d0d2414de0772536ffb5103d
plato-cambrian <plato@cambrian.org>
2014-09-11 14:26:07 -0600

    Remove unused icons with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,24 @@ module.exports = function(grunt) {
         }
       },
     },
-    clean: ["node_modules", 
+    clean:[ "node_modules", 
             "test", 
             ".git",
             ".gitignore", 
             "package.json", 
             "bower.json",
-           ],
+            // Remove all material-icons except these exceptions:
+            "material-icons",
+            "!material-icons/icons/system_icons/navigation/res/1x_web/ic_menu_wht_48dp.png",
+            "!material-icons/icons/system_icons/action/res/1x_web/ic_view_stream_wht_48dp.png",
+            "!material-icons/icons/system_icons/action/res/1x_web/ic_view_quilt_wht_48dp.png",
+            "!material-icons/icons/system_icons/social/res/1x_web/ic_group_18dp.png",
+            "!material-icons/icons/system_icons/action/res/1x_web/ic_drawer_wht_18dp.png",
+            "!material-icons/icons/system_icons/social/res/2x_web/ic_group_48dp.png",
+            "!material-icons/icons/system_icons/social/res/2x_web/ic_person_48dp.png",
+            "!material-icons/icons/system_icons/navigation/res/1x_web/ic_arrow_forward_24dp.png",
+            "!material-icons/icons/system_icons/navigation/res/1x_web/ic_arrow_back_24dp.png",
+          ],
     gta: {
       add_target: {
         command: 'submodule update --init',
