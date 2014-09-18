addc94a592912d1ebfcf4bc3064b6ff3da22a40c
plato-cambrian <plato@cambrian.org>
2014-09-17 20:56:57 -0600

    Rearrange dialog and elements

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ if(Cambrian.JAPI !== undefined && !Cambrian.isMockCambrian){
 
 //appModule = angular.module("app", ['eee-c.angularBindPolymer'])
 
-var GROUP_TYPES = ['broadcast', 'open'];
+var GROUP_TYPES = ['corporation', 'broadcast', 'open'];
 
 appModule = angular.module("app", ['ngMaterial'])
 .factory("menu", ['$rootScope', function ($rootScope) {
@@ -41,6 +41,14 @@ appModule = angular.module("app", ['ngMaterial'])
   $scope.myPeerLists = japi.me.peerLists;
   $scope.groupTypes = GROUP_TYPES;
   $scope.newGroupType = $scope.groupTypes[0];
+  /*
+  $scope.isEditing = false;
+  $scope.tempGroup = {
+    name: "",
+    type: "",
+    description: "",
+  }
+  */
 
   for (var i=0; i < $scope.myGroups.length; i++) {
     $scope.myGroups[i].isActive = false;
@@ -51,6 +59,26 @@ appModule = angular.module("app", ['ngMaterial'])
     $scope.myGroups.isActive[i] = false;
   };
   */
+
+  $scope.openEditorPhase1 = function(){
+    $scope.isEditingPhase1 = true;
+    $scope.isEditingPhase2 = false;
+    //var nTypes = GROUP_TYPES.length;
+    //angular.element('#quickAddBox select').attr('size', Math.min(nTypes, 10));
+  };
+
+  $scope.openEditorPhase2 = function(){
+    $scope.isEditingPhase1 = false;
+    $scope.isEditingPhase2 = true;
+    $scope.newGroup()
+  };
+
+  $scope.stopEditing = function(){
+    //angular.element('#quickAddBox select').attr('size', 1);
+    $scope.isEditingPhase1 = false;
+    $scope.isEditingPhase2 = false;
+
+  }
 
   $scope.toggleMenu = function () {
     $materialSidenav('left').toggle();
@@ -70,16 +98,45 @@ appModule = angular.module("app", ['ngMaterial'])
     group.overflow = !group.overflow;
   };
 
+  // We will now invoke newGroup between isEditingPhase1 and isEditingPhase2
   $scope.newGroup = function () {
     //var groupToAdd = { name: $scope.newGroupTitle, groupType: $scope.newGroupType, members: []};
-    var groupToAdd = japi.groups.build('open');
+    var groupToAdd = japi.groups.build($scope.newGroupType);
     groupToAdd.name = $scope.newGroupTitle;
-    groupToAdd.type = $scope.newGroupType;
-    groupToAdd.members = [];
+    //groupToAdd.type = $scope.newGroupType;
+    //groupToAdd.members = [];
     $scope.myGroups.push(groupToAdd);
     $scope.newGroupTitle = "";
     $scope.quickAddForm.$setPristine();
-    $scope.dialog(null, groupToAdd);
+    // We'll craft a dummy event from the quick add box so the dialog animates nicely
+    var qab = angular.element('#quickAddBox');
+    var x = qab.offset().left;
+    var y = qab.offset().top;
+    /*
+    qab.click(function(){
+      $scope.dialog(e, groupToAdd);
+    });
+    qab.click();
+    */
+
+
+    console.log(qab);
+    //var e = new Event('click');
+    var e = document.createEvent('MouseEvents');
+    e.initMouseEvent(
+      'click',
+      false, false, window,       // canBubble, cancelable, view
+      0, 300, 300, 300, 300,      // detail, screenX, screenY, clientX, clientY
+      false, false, false, false, // ctrlKey, altKey, shiftKey, metaKey
+      0, null                     // button(LMB), relatedTarget
+    )
+    console.log(e);
+    qab.click(function(e){
+      console.log(e)
+    });
+
+    //qab.dispatchEvent()
+    $scope.dialog(e, groupToAdd);
   };
 
   $scope.duplicateGroup = function (group) {
@@ -108,6 +165,9 @@ appModule = angular.module("app", ['ngMaterial'])
     $materialDialog({
       templateUrl: 'partials/editGroupCard.tmpl.html',
       targetEvent: e,
+      hasBackdrop: false,
+      //clickOutsideToClose: false, // we have to do something custom
+      //escapeToClose: false,
       controller: ['$scope', '$hideDialog', function ($scope, $hideDialog) {
         $scope.group = group;
         $scope.japi = japi;
@@ -115,6 +175,13 @@ appModule = angular.module("app", ['ngMaterial'])
         console.log('Setting dialog $scope.newGroupType to ',group.type)
         $scope.groupTypes = GROUP_TYPES;
         $scope.newGroupTitle = group.name;
+
+        // Focus the group name when the dialog opens:
+        // ACTUALLY. the material-dialog script focuses the close button.
+        // Ugly hack:
+        window.setTimeout(function(){
+          angular.element('material-dialog #groupName').focus();
+        }, 200);
 
         // Filter to display only nonMembers:
         $scope.nonMembers = function(peer){
@@ -127,14 +194,25 @@ appModule = angular.module("app", ['ngMaterial'])
         };
         $scope.close = function () {
           $hideDialog();
+          $scope.stopEditing();
         };
 
         $scope.save = function (group) {
           group.save()
           $hideDialog();
+          $scope.stopEditing();
         };
+        /* No good, this is not in scope...
+        $scope.$on("$destroy", function(){
+          $scope.stopEditing();
+        });
+        */
       }]
     });
   };
 
 });
+
+window.setInterval(function(){
+  console.log(document.activeElement);
+}, 5000);
